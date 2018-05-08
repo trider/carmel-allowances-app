0f6d2072ccd79d060a319b5b58b2c9cd43f1e4c9
Jonathan Gold <jonnygold@gmail.com>
2018-05-08 12:47:06 +0300

    step-1 Ver 0.4: Added config

--- .history/src/index_20180507152040.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-// import 'bootstrap/dist/css/bootstrap.css';
-import registerServiceWorker from './lib/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
